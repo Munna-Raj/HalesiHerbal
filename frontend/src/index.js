501791994f60676101477ff7007e6a0cdade72ad
Munna-Raj <my2056875@gmail.com>
2026-04-25 22:03:19 +0545

    deploying project

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 import App from './App';
 import './index.css';
 import { ThemeProvider } from './context/ThemeContext';
+
+// Set axios base URL from environment variable
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
